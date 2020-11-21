043c5a676981e1b88c594560d0500df437a595aa
Hira0826 <74807670+Hira0826@users.noreply.github.com>
2020-11-21 11:09:41 +0500

    Create adminview.php

---
+++ blogadmin/adminview.php
@@ -0,0 +1,32 @@
+ <div class="row">
+          
+          
+      
+          <div class="col-lg-3">
+            <div class="panel panel-primary">
+              <div class="panel-heading">
+                <div class="row">
+                  <div class="col-xs-6">
+                    <i class="fa fa-bar-chart-o fa-5x"></i>
+                  </div>
+                  <div class="col-xs-6 text-right">
+                    <p class="announcement-heading"><?php adminstats("visitor_info","page_hits");?></p>
+                    <p class="announcement-text"><strong>Admin Stats</strong></p>
+                  </div>
+                </div>
+              </div>
+              <a href="../adminstats">
+                <div class="panel-footer announcement-bottom">
+                  <div class="row">
+                    <div class="col-xs-6">
+                      View
+                    </div>
+                    <div class="col-xs-6 text-right">
+                      <i class="fa fa-arrow-circle-right"></i>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div><!-- /.row -->
